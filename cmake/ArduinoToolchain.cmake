458e4d6d29cb237da57c57a68c083bdf41542f19
Deep Dhillon <deep@deepdhillon.ca>
2018-10-14 01:48:30 -0400

    Updated cmake toolchain file

--- cmake/ArduinoToolchain.cmake
+++ cmake/ArduinoToolchain.cmake
@@ -1,2 +1,2 @@
 set(ARDUINO_SDK_PATH "${CMAKE_CURRENT_LIST_DIR}/../core/arduino")
-include ({{ARDUINO_CMAKE_PATH}}/cmake/ArduinoToolchain.cmake")
+include ("${CMAKE_CURRENT_LIST_DIR}/../../../module-arduino-cmake@1.0.0/cmake/ArduinoToolchain.cmake")
