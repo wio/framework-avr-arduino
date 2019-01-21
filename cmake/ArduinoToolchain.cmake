668b7901c97e1e0b8f6019578b78db9fdecf36e0
Deep Dhillon <deep@deepdhillon.ca>
2019-01-21 15:24:13 -0500

    fix 1.0.2

--- cmake/ArduinoToolchain.cmake
+++ cmake/ArduinoToolchain.cmake
@@ -1,5 +1,5 @@
 # provided by wio
 include("${CMAKE_CURRENT_LIST_DIR}/../WioConfig.cmake")
 
-set(ARDUINO_SDK_PATH "${CMAKE_CURRENT_LIST_DIR}/../core")
-include ("${WIO_TOOLCHAIN_PATH}/module-arduino-cmake__1.0.0/cmake/ArduinoToolchain.cmake")
+set(ARDUINO_SDK_PATH "${WIO_CURRENT_TOOLCHAIN_PATH}/core")
+include ("${WIO_DEP_WIO-MODULE-ARDUINO-CMAKE_PATH}/cmake/ArduinoToolchain.cmake")
